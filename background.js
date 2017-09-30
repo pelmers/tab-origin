bc5d75f475b846ea8930221e95997c929d54e5b5
Peter Elmers <peter.elmers@yahoo.com>
2017-09-30 13:12:35 -0700

    As backup for openerTabId also track the last active tab

--- background.js
+++ background.js
@@ -1,10 +1,7 @@
-// Firefox = browser, Chrome = chrome.
-let api;
-try {
-    api = chrome;
-} catch(e) {
-    api = browser;
-}
+// TODO: port to browser api
+const api = chrome;
+
+const activeTabKey = "activeTab";
 
 // Return the last element in an array.
 // If the array is empty, return null.
@@ -43,35 +40,60 @@ function openTabOrigin(tab) {
     });
 }
 
+function updateOpenerState(newTab, openerTab) {
+    const match_id = openerTab.id.toString();
+    const tab_id = newTab.id.toString();
+    // We set the new tab's history stack to
+    // [original tab's history] + original tab's URL.
+    // By retaining the entire history stack, we can do the nifty trick of
+    // keeping tab origin state across repeated invocations, allowing us to tab
+    // origin our way all the way back to the first tab opened.
+    api.storage.local.get(match_id, function (result) {
+        const match_stack = result[match_id] || [];
+        // The query API does not match on hash mark, so strip that off.
+        if (openerTab.url.lastIndexOf("#") > -1) {
+            const base = openerTab.url.substr(0, openerTab.url.lastIndexOf("#"));
+            api.storage.local.set({ [tab_id]: match_stack.concat([base]) });
+        } else {
+            api.storage.local.set({ [tab_id]: match_stack.concat([openerTab.url]) });
+        }
+    });
+
+}
+
 api.browserAction.onClicked.addListener(openTabOrigin);
 
+// Remark: on new tab creation, we receive onCreated before onActivated.
+api.tabs.onActivated.addListener(info => {
+    const id = info.tabId;
+    api.storage.local.set({[activeTabKey]: id});
+});
+
 api.tabs.onCreated.addListener(function(tab) {
+    console.log("CREATED!", tab.id);
     if (tab.openerTabId !== undefined) {
         api.tabs.get(tab.openerTabId, function(match) {
             if (match !== undefined) {
-                const match_id = match.id.toString();
-                const tab_id = tab.id.toString();
-                // We set the new tab's history stack to
-                // [original tab's history] + original tab's URL.
-                // By retaining the entire history stack, we can do the nifty trick of
-                // keeping tab origin state across repeated invocations, allowing us to tab
-                // origin our way all the way back to the first tab opened.
-                api.storage.local.get(match_id, function(result) {
-                    const match_stack = result[match_id] || [];
-                    // The query API does not match on hash mark, so strip that off.
-                    if (match.url.lastIndexOf("#") > -1) {
-                        const base = match.url.substr(0, match.url.lastIndexOf("#"));
-                        api.storage.local.set({[tab_id]: match_stack.concat([base])});
-                    } else {
-                        api.storage.local.set({[tab_id]: match_stack.concat([match.url])});
-                    }
-                });
+                updateOpenerState(tab, match);
             } else {
-                console.log("Could not find opener for tab " + tab.url);
+                console.log("Opener is defined, but I can't find it for " + tab.url);
             }
         });
     } else {
-        console.log("ㅠㅠ Undefined opener for tab " + tab.url + " ㅠㅠ");
+        //TODO: file a bug!
+        // in firefox openerTabId is not set for tabs opened by alt-enter in address bar.
+        // (it's only set for tabs opened from links on the page)
+        api.storage.local.get(activeTabKey, tabId => {
+            console.log("actiavet tab is...", tabId);
+            api.tabs.get(tabId[activeTabKey], match => {
+                console.log(match);
+                if (match !== undefined) {
+                    updateOpenerState(tab, match);
+                } else {
+                    console.log("Attempted to use active tab as opener, but it was not set.");
+                }
+            });
+        });
     }
 })
 
