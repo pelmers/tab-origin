13497a486680a40c4949317e59f87ad9d28ce010
T1mL3arn <T1mL3arn@users.noreply.github.com>
2023-10-11 23:18:50 +1000

    fix indentation

--- background.js
+++ background.js
@@ -14,9 +14,9 @@ let currentActionType;
 // Return the last element in an array.
 // If the array is empty, return null.
 function last(array) {
-	if (!array || array.length === 0)
-		return null;
-	return array[array.length - 1];
+    if (!array || array.length === 0)
+        return null;
+    return array[array.length - 1];
 }
 
 api.storage.local.onChanged.addListener(changes => {
@@ -104,59 +104,59 @@ function extensionButtonClick(tab) {
 }
 
 function updateOpenerState(newTab, openerTab) {
-	const match_id = openerTab.id.toString();
-	const tab_id = newTab.id.toString();
-	// We set the new tab's history stack to
-	// [original tab's history] + original tab's URL.
-	// By retaining the entire history stack, we can do the nifty trick of
-	// keeping tab origin state across repeated invocations, allowing us to tab
-	// origin our way all the way back to the first tab opened.
-	api.storage.local.get(match_id, function (result) {
-		const match_stack = result[match_id] || [];
-		// The query API does not match on hash mark, so strip that off.
-		if (openerTab.url.lastIndexOf("#") > -1) {
-			const base = openerTab.url.substr(0, openerTab.url.lastIndexOf("#"));
-			api.storage.local.set({ [tab_id]: match_stack.concat([base]) });
-		} else {
-			api.storage.local.set({ [tab_id]: match_stack.concat([openerTab.url]) });
-		}
-	});
+    const match_id = openerTab.id.toString();
+    const tab_id = newTab.id.toString();
+    // We set the new tab's history stack to
+    // [original tab's history] + original tab's URL.
+    // By retaining the entire history stack, we can do the nifty trick of
+    // keeping tab origin state across repeated invocations, allowing us to tab
+    // origin our way all the way back to the first tab opened.
+    api.storage.local.get(match_id, function (result) {
+        const match_stack = result[match_id] || [];
+        // The query API does not match on hash mark, so strip that off.
+        if (openerTab.url.lastIndexOf("#") > -1) {
+            const base = openerTab.url.substr(0, openerTab.url.lastIndexOf("#"));
+            api.storage.local.set({ [tab_id]: match_stack.concat([base]) });
+        } else {
+            api.storage.local.set({ [tab_id]: match_stack.concat([openerTab.url]) });
+        }
+    });
 
 }
 
 // Remark: on new tab creation, we receive onCreated before onActivated.
 api.tabs.onActivated.addListener(info => {
-	const id = info.tabId;
-	api.storage.local.set({ [activeTabKey]: id });
+    const id = info.tabId;
+    api.storage.local.set({[activeTabKey]: id});
 });
 
-api.tabs.onCreated.addListener(function (tab) {
-	if (tab.openerTabId !== undefined) {
-		api.tabs.get(tab.openerTabId, function (match) {
-			if (match !== undefined) {
-				updateOpenerState(tab, match);
-			} else {
-				console.log("Opener is defined, but I can't find it for " + tab.url);
-			}
-		});
-	} else {
-		//TODO: file a bug!
-		// in firefox openerTabId is not set for tabs opened by alt-enter in address bar.
-		// (it's only set for tabs opened from links on the page)
-		api.storage.local.get(activeTabKey, tabId => {
-			api.tabs.get(tabId[activeTabKey], match => {
-				if (match !== undefined) {
-					updateOpenerState(tab, match);
-				} else {
-					console.log("Attempted to use active tab as opener, but it was not set.");
-				}
-			});
-		});
-	}
+api.tabs.onCreated.addListener(function(tab) {
+    if (tab.openerTabId !== undefined) {
+        api.tabs.get(tab.openerTabId, function(match) {
+            if (match !== undefined) {
+                updateOpenerState(tab, match);
+            } else {
+                console.log("Opener is defined, but I can't find it for " + tab.url);
+            }
+        });
+    } else {
+        //TODO: file a bug!
+        // in firefox openerTabId is not set for tabs opened by alt-enter in address bar.
+        // (it's only set for tabs opened from links on the page)
+        api.storage.local.get(activeTabKey, tabId => {
+            api.tabs.get(tabId[activeTabKey], match => {
+                if (match !== undefined) {
+                    updateOpenerState(tab, match);
+                } else {
+                    console.log("Attempted to use active tab as opener, but it was not set.");
+                }
+            });
+        });
+    }
 })
 
-api.tabs.onRemoved.addListener(function (tabId) {
-	api.storage.local.set({ [tabId.toString()]: [] });
+api.tabs.onRemoved.addListener(function(tabId) {
+    api.storage.local.set({[tabId.toString()]: []});
 });
 
 // read initial action type
